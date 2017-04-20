bf5d515f1c63976f70831ecb38ef9d8a5f5ee419
Howard Jiang <howard.yunghao.jiang@gmail.com>
2017-04-19 17:13:37 -0700

    init

---
+++ server/config/routes.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+
+module.exports = (app) => {
+    // This route is just for the default welcome page.
+    app.get('/', (req, res) => {
+        // Default Welcome Message.
+        res.send('<h1>Hey there!</h1><h3>Project: hubstack_backend</h3><h4>A few things to know....</h4><ul><li>Your server is up and running...</li><li>Routes are restful and set up...</li><li>Controllers are set up per model...</li></ul><p><i>Made by Howard Jiang (github.com/HowiJ/node_gen_bash)</i></p>')
+    })
+}
